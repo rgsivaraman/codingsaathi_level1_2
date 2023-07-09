9fbd6fcdad0fdea7d981ec22edfa3e79b592ec22
SIVARAMAN G <99677088+rgsivaraman@users.noreply.github.com>
2023-07-09 18:01:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Form submission
+const contactForm = document.getElementById('contact-form');
+contactForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+});
